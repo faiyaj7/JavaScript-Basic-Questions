4caa51680fd1a36c406da887c5bffb2657006b19
faiyaj7 <faiyaj.bin.ahmed@gmail.com>
2024-05-04 19:54:04 +0600

    Assignment done

---
+++ index.js
@@ -0,0 +1,64 @@
+// 1.
+
+// Two ways to connect
+// First one is Inline Javascript:
+{
+  /* 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JavaScript Number 1 Question</title>
+  </head>
+  <body> 
+    <script>
+      console.log("This is the inline way of connecting HTML and Js");
+    </script>
+  </body>
+ 
+</html> 
+*/
+}
+
+// Second one is External Javascript
+// Creating a different js file and attaching it with the desired html file
+{
+  /* 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JavaScript Number 1 Question</title>
+  </head>
+  <body>
+    <script src="./index.js">
+    </script>
+  </body>
+</html> */
+}
+
+// index.js
+// console.log("This is the external way of connecting HTML and Js")
+
+// 2.
+// var name="Faiyaj Bin Ahmed"
+// let course="MERN Stack"
+// const task="Javacript Basic Questions"
+
+// 3.
+// var: It can be declared without initialization and can be updated and re-declared within the same scope.
+// let : It can be declared without initialization and can be updated but cannot be re-declared within the same scope
+// const : It cannot be declared without initialization and cannot be updated and re-declared in any type of scope.
+
+// 4.
+// console.log("Faiyaj Bin Ahmed")
+
+// 5.
+// alert("Faiyaj Bin Ahmed")
+
+// 6.
+// let firstNumber = 23;
+// let secondNumber = 24;
+// console.log(firstNumber + secondNumber)
